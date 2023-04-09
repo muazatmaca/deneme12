2002040deeb9c8ee5ba13182f74816186534079f
muazatmaca <muaz5447@gmail.com>
2023-04-09 22:59:20 +0300

    frame oluşturuldu 5

--- src/test/java/Page/denemesayfa.java
+++ src/test/java/Page/denemesayfa.java
@@ -3,5 +3,6 @@ package Page;
 public class denemesayfa {
     public static void main(String[] args) {
         System.out.println("merhaba ddd");
+
     }
 }
--- src/test/java/Utility/GWD.java
+++ src/test/java/Utility/GWD.java
@@ -2,4 +2,5 @@ package Utility;
 
 public class GWD {
 
+
 }
